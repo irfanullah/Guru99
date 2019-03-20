6b422d0cd73d67e0c2940f795f3e684634bcb7e0
irfanullah <raiirfan@gmail.com>
2019-03-20 14:07:43 +0500

    removed conflicts

---
+++ src/main/java/T23_TestNG.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class T23_TestNG {
+    public String baseUrl = "http://demo.guru99.com/test/newtours/";
+    public WebDriver driver;
+
+    @Test
+    public void verifyHomepageTitle(){
+        
+    }
+}
